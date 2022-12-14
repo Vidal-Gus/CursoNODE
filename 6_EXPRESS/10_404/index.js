a0e759cf2fa1ee1e52c47cf6883f41b29019c124
Vidal-Gus <vidal47pro@gmail.com>
2022-12-14 14:56:52 -0300

    finalizando introducao ao express

---
+++ 6_EXPRESS/10_404/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+const path = require('path')
+
+const basePath = path.join(__dirname, 'templates')
+
+const users = require('./users')
+
+//lerbody
+app.use(
+    express.urlencoded({
+        extended: true
+    }),
+)
+
+app.use(express.json())
+
+//arquivos estáticos
+app.use(express.static('public'))
+
+app.use('/users', users)
+
+app.get('/',(req,res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.use(function(req,res,next){
+    res.status(404).sendFile(`${basePath}/404.html`)
+})
+
+app.listen(port
+    ,() =>{
+        console.log(`Servidor aberto na porta ${port}`)
+})
---
+++ 6_EXPRESS/8_modulo_rotas/users/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+const basePath = path.join(__dirname, '../templates')
+
+//lerbody
+router.use(
+    express.urlencoded({
+        extended: true
+    }),
+)
+
+router.use(express.json())
+
+router.get('/add', (req,res) => {
+    res.sendFile(`${basePath}/userform.html`)
+})
+
+router.post("/save", (req,res) => {
+    console.log(req.body)
+    const name = req.body.name
+    const age = req.body.age
+
+    console.log(`O nome do usuario é ${name} e a idade é ${age}`)
+
+    res.sendFile(`${basePath}/userform.html`)
+})
+
+router.get('/:id',(req,res) => {
+    const id = req.params.id
+
+    //leitura para resgatar usuarios
+    console.log(`Estamos buscando pelo usuario ${id}`)
+
+    res.sendFile(`${basePath}/users.html`)
+})
+
+module.exports = router
